a30c46efc96fb5643cbe052341c55585d764a7bc
Vachia Thoj <47841533+ViseROM@users.noreply.github.com>
2023-12-04 17:21:22 -0600

    Add files via upload

--- src/helper/NumberGenerator.java
+++ src/helper/NumberGenerator.java
@@ -11,15 +11,15 @@ import java.util.Random;
 public class NumberGenerator 
 {
 	/**
-	 * Method that generates a random number
-	 * @param min (integer) the smallest possible number to generate
-	 * @param max (integer) the largest possible number to generate
+	 * Method that generates a random integer number
+	 * @param min (integer) the smallest possible number to generate (inclusive)
+	 * @param max (integer) the largest possible number to generate (inclusive)
 	 * @return number (integer) a random number
 	 */
 	public static int getRandomNumber(int min, int max)
 	{
 		Random random = new Random();
-		int number = random.nextInt(max) + min;
+		int number = random.nextInt((max - min) + 1) + min;
 		
 		return number;
 	}
